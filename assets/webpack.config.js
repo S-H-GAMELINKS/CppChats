8596651f5ae15f56eab105ffeebe57a494544d13
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-27 22:56:13 +0900

    using .vue in webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     module: {
         rules: [
         {
+            test: /\.vue$/,
+            use: 'vue-loader'
+        },
+        {
             test: /\.css$/,
             use: [
                 'style-loader',
