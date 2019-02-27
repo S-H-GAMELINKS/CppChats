e7705903364d6a27bd752afb70059ee7e4a1a4d6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-27 23:54:46 +0900

    fix using Header

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Header from '../components/layouts/header.vue';
+import Header from '../components/layouts/Header.vue';
 import * as  Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
