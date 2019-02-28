4948d0d517cf8ab03c7b5d34120fca7cc71598da
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-28 09:37:24 +0900

    using vue-router

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Header from '../components/layouts/Header.vue';
+import Router from '../router/index';
 import * as  Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -7,6 +8,7 @@ Vue.use(Bootstrap);
 
 const app = new Vue({
     el: ".app",
+    router: Router,
     components: {
         'nav-bar': Header
     }
