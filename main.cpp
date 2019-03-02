db099e4ff4f1fbfe21fc59cc6cbc4fe4deae0855
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-02 23:39:15 +0900

    add /api/talks

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <array>
+#include <vector>
 
 const std::string load_assets(const std::string& path) {
 
@@ -29,6 +30,18 @@ int main() {
     std::string indexjs = load_assets("./assets/index.js");
 
     std::array<std::string, 3> routes = {"/", "/about", "/contact", "/chat"};
+
+    std::vector<std::string> chats;
+
+    svr.Get("/api/talks", [&](const httplib::Request& req, httplib::Response& res) {
+
+        std::ostringstream chat;
+
+        for(auto&& c : chats)
+            chat << "<p>" << c.c_str() << "</p>";
+
+        res.set_content(chat.str(), "text/plain");
+    });
 
     for(auto&& r : routes)
         svr.Get(r.c_str(), [&](const httplib::Request& req, httplib::Response& res) {
