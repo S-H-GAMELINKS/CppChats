32c3f52e206362e5568708b264143ff80bb791ad
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-02 23:49:49 +0900

    using stimulus

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,6 +4,13 @@ import Router from '../router/index';
 import * as  Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 Vue.use(Bootstrap);
 
 const app = new Vue({
